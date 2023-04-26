6b2b213c76c5f0696ebfb8637c694db6a6f4f6fe
Dxxelisson2 <130613973+Dxxelisson2@users.noreply.github.com>
2023-04-26 19:37:59 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2_ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2;
+
+            Console.Write("digita o primeiro numero: ");
+            n1 = double.Parse(Console.ReadLine());
+
+            Console.Write("digite o segundo numero: ");
+
+            n2 = double.Parse(Console.ReadLine());
+            
+                if (n1 == n2)
+
+                Console.WriteLine("os numeros são iguais.");
+            
+            else         
+            
+                if (n1 > n2)
+
+                Console.WriteLine("o primeiro é maior que o segundo."); 
+            
+            else
+            Console.WriteLine("o segundo e maior que o primeiro.");
+            
+        }
+    }
+}
